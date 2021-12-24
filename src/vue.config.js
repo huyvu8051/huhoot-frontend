3662a30649004eeabb2f7e8b7024f18d35443b75
huyvu8051 <huyvu8051@gmail.com>
2021-12-24 23:33:34 +0700

    d

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    backendUrl: "http://localhost:8081",
+    backendUrl: "https://localhost:8081",
     socketUrl: "http://localhost:8082"
     //backendUrl: "http://159.223.38.181:8081",
     //socketUrl: "http://159.223.38.181:8082"
