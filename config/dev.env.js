bddcfd7db2ab50497c2dd8702e699cb880204691
huyvu8051 <huyvu8051@gmail.com>
2022-06-04 05:19:21 +0700

    change local ip

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,7 @@
 "use strict";
 const merge = require("webpack-merge");
 const prodEnv = require("./prod.env");
-const localIp = "192.168.1.19";
+const localIp = "localhost";
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   BACKEND_URL: `"http://${localIp}:8081"`,
